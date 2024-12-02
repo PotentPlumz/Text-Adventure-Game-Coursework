3a248d902971e0da84a4a5541486f082ff722084
ecce1 <ecce1@MORGANS_LAPTOP>
2024-12-02 14:17:51 +0000

    Add project files.

---
+++ coursework_project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace coursework_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
