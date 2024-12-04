c2f1e81ae16c6e39d7abf87a2cb9dd7ffd8e76e1
Morgan Ecclestone <N0786934@my.ntu.ac.uk>
2024-12-04 15:13:30 +0000

    comitting after completing main menu selection logic. Decided to go with colour changing to indicate the user's position in the main menu rather than an arrow.
    Code ideally needs amending so that it can handle differing numbers of menu options and turned into a function within itself as this function is going to be repeated several times throughout the game.

--- coursework_project/Menus.cs
+++ coursework_project/Menus.cs
@@ -8,6 +8,8 @@ namespace coursework_project
     {
         static public void welcome_screen()
         {
+
+            //Make sure to follow copyright laws and credit the authors for the Asc key art and sounds/music
             SoundPlayer sound = new SoundPlayer("main_menu_music.wav");
             sound.Play();
             Console.WriteLine("""\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\""");
@@ -16,14 +18,14 @@ namespace coursework_project
             for (int i = 0; i < message.Length; i++)
             {
                 Console.Write(message[i]);
-                Thread.Sleep(100);
+                Thread.Sleep(20);
             }
             Console.WriteLine();
             Console.WriteLine("""\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\""");
         }
         static public void main_menu()
         {
-
+            Console.CursorVisible = false;
             Console.WriteLine("\nPlease use the Up and Down arrows to select an option\n");
             Console.WriteLine("   \x1B[4mMain Menu\x1B[0m");
 
@@ -34,19 +36,42 @@ namespace coursework_project
 
             ConsoleKeyInfo user_keypress;
 
+
+            string red_text_colour = "\x1b[1;31m";
+            string default_colour_code = "\u001b[0m";
+            string black_text_colour = "\x1b[1; 31m";
+
             while (selected_item == false)
             {
                 Console.SetCursorPosition(cursor_menu_hori, cursor_menu_vert);
 
-                Console.WriteLine("1) New Game" + " <--");
-                Console.WriteLine("2) Load Game");
-                Console.WriteLine("3) Exit");
+                Console.WriteLine($"1) {(menu_position == 1 ? red_text_colour : "")}New Game" + default_colour_code);
+                Console.WriteLine($"2) {(menu_position == 2 ? red_text_colour : "")}Load Game"+ default_colour_code);
+                Console.WriteLine($"3) {(menu_position == 3 ? red_text_colour : "")}Options" + default_colour_code);
+                Console.WriteLine($"4) {(menu_position == 4 ? red_text_colour : "")}Exit" + default_colour_code);
 
                 user_keypress = Console.ReadKey();
 
+                if (user_keypress.Key == ConsoleKey.DownArrow)
+                {
+                    menu_position++;
+                    if (menu_position == 5)
+                        menu_position = 1;
+                }
+                if (user_keypress.Key == ConsoleKey.UpArrow)
+                {
+                    menu_position--;
+                    if (menu_position == 0)
+                        menu_position = 4;
+                }
+                if (user_keypress.Key == ConsoleKey.Enter)
+                {
+                    selected_item = true;
+                    Console.WriteLine($"You have selected option {menu_position}");
+                }
+
 
             }
-            
 
         }
     }
