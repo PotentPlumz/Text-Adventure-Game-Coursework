5b4ca40817518efa8b71c520eac18c62bde5cf1e
Morgan Ecclestone <N0786934@my.ntu.ac.uk>
2025-01-17 12:57:55 +0000

    True final commit after adding Github URL commented on main program file

--- coursework_project/Program.cs
+++ coursework_project/Program.cs
@@ -6,25 +6,18 @@
         public static Player current_player = new Player();
         public static List<string> no_art_list = new List<string>();
 
+        //My Github URL = https://olympus.ntu.ac.uk/N0786934/Visual_Novel.git
+
         //This is default scroll speed if config is corrupt
         public static int scroll_speed = 20;
         static public void Main()
         {
             //Allows for use of unicode chars
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             File_Load.Check_files_proceedure();
 
-
-   
-
            welcome_main_menu();
-
-
-
-
-
-           
         }
         static public void welcome_main_menu()
         {
